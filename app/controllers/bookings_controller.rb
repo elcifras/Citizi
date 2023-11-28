d43ec054d3a543ed4142b052f6971129996d954b
Imanol Barrios <imanolbarrios1993@gmail.com>
2023-11-28 17:37:33 +0100

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,51 @@
 class BookingsController < ApplicationController
+  before_action :set_service, only: %i[create]
+  before_action :authenticate_user!, only: %i[create index]
+  
   def index
-    @bookings = Booking.where(user: current_user)
+    if current_user.is_hotel
+      @bookings = current_user.accepted_bookings
+    else
+      @bookings = current_user.bookings
+    end
   end
 
   def create
+    unless current_user.is_hotel
+      @booking = Booking.new(booking_params)
+      @booking.user = current_user
+      @booking.timeslot = Timeslot.find(params[:timeslot_id]) # missing the front end to b able to select a tiemslot
+      @booking.status = "Confirmed"
+      if @booking.save
+        redirect_to bookings_path, notice: "Booking created"
+      else
+        redirect_to service_path(@service), notice: "Problem to book"
+      end
+    else
+      redirect_to bookings_path, notice: "You can not book"
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    if @booking.destroy
+      redirect_to bookings_path, notice: "Booking deleted"
+    else
+      redirect_to bookings_path, status: :unprocessable_entity, notice: "Booking not deleted"
+    end
+  end
+
+  private
+
+  def set_service
+    @service = service.find(params[:service_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:occurs_on, :timeslot_id, :status, :service_id)
+  end
+
+  def is_hotel?
+    current_user.is_hotel
   end
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,3 +1,4 @@
 class Timeslot < ApplicationRecord
   belongs_to :service
+  has_one :booking
 end
