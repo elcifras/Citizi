053f43b290fe5273106cfbb8cab1792cc3f67779
Gaelle Vivet <ga.vivet@gmail.com>
2023-11-29 16:40:26 +0100

    search functionality added on home

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,9 @@ class User < ApplicationRecord
   has_many :timeslots, through: :services
   has_many :accepted_bookings, through: :timeslots, source: :booking
   has_many :reviews, through: :bookings
+
+  scope :is_hotel, -> { where(is_hotel: true) }
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
