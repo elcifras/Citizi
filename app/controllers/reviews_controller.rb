7150470455d29ad56d465a9e5776f129601915ec
Gaelle Vivet <ga.vivet@gmail.com>
2023-12-04 17:31:07 +0100

    tab bookings are working and working on reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :set_booking, only: %i[new create]
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    if @review.save
+      redirect_to @bookings_path, notice: 'Review was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :comment, presence: true
 end
