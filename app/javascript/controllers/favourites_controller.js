6c4b5463f1152ab2703b3a63dfbfb9d5a9082987
Amina Ghafor <aminarghafor@gmail.com>
2023-12-06 10:50:30 +0100

    favourites-2

--- app/javascript/controllers/favourites_controller.js
+++ app/javascript/controllers/favourites_controller.js
@@ -5,37 +5,39 @@ export default class extends Controller {
   connect() {
   }
 
-//   addFavourite(event) {
-//     event.preventDefault()
-//     const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-//     const favouriteButton = event.currentTarget
-//     const itemId = favouriteButton.dataset.itemId
-//     const requestInfo = {
-//       method: "POST",
-//       headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
-//     }
+  addFavourite(event) {
+    event.preventDefault()
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    const favouriteButton = event.currentTarget
+    const serviceId = favouriteButton.dataset.serviceId
+    // is this correct?
+    const requestInfo = {
+      method: "POST",
+      headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
+    }
 
-//     fetch(`/favourites?item_id=${itemId}`, requestInfo)
-//       .then(response => response.json())
-//       .then((data) => {
-//         this.element.outerHTML = data.inserted_item
-//       })
-//   }
+    fetch(`/favourites?service_id=${serviceId}`, requestInfo)
+      .then(response => response.json())
+      .then((data) => {
+        this.element.outerHTML = data.inserted
+      })
+  }
 
-//   deleteFavourite(event) {
-//     event.preventDefault()
-//     const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-//     const favouriteButton = event.currentTarget
-//     const itemId = favouriteButton.dataset.itemId
-//     const requestInfo = {
-//       method: "DELETE",
-//       headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
-//     }
+  deleteFavourite(event) {
+    event.preventDefault()
+    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    const favouriteButton = event.currentTarget
+    const serviceId = favouriteButton.dataset.serviceId
+    //serviceId? not being used in the controller, is it just service?
+    const requestInfo = {
+      method: "DELETE",
+      headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
+    }
 
-//     fetch(`/favourites/${itemId}`, requestInfo)
-//       .then(response => response.json())
-//       .then((data) => {
-//         this.element.outerHTML = data.inserted_item
-//       })
-//   }
-// }
+    fetch(`/favourites/${serviceId}`, requestInfo)
+      .then(response => response.json())
+      .then((data) => {
+        this.element.outerHTML = data.inserted
+      })
+  }
+}
