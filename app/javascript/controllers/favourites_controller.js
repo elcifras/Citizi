820398e586fd6c6dfce24cef08dc1186f0bd244a
Amina Ghafor <aminarghafor@gmail.com>
2023-12-06 09:28:19 +0100

    favourites

--- app/javascript/controllers/favourites_controller.js
+++ app/javascript/controllers/favourites_controller.js
@@ -5,37 +5,37 @@ export default class extends Controller {
   connect() {
   }
 
-  addFavourite(event) {
-    event.preventDefault()
-    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    const favouriteButton = event.currentTarget
-    const itemId = favouriteButton.dataset.itemId
-    const requestInfo = {
-      method: "POST",
-      headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
-    }
+//   addFavourite(event) {
+//     event.preventDefault()
+//     const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+//     const favouriteButton = event.currentTarget
+//     const itemId = favouriteButton.dataset.itemId
+//     const requestInfo = {
+//       method: "POST",
+//       headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
+//     }
 
-    fetch(`/favourites?item_id=${itemId}`, requestInfo)
-      .then(response => response.json())
-      .then((data) => {
-        this.element.outerHTML = data.inserted_item
-      })
-  }
+//     fetch(`/favourites?item_id=${itemId}`, requestInfo)
+//       .then(response => response.json())
+//       .then((data) => {
+//         this.element.outerHTML = data.inserted_item
+//       })
+//   }
 
-  deleteFavourite(event) {
-    event.preventDefault()
-    const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    const favouriteButton = event.currentTarget
-    const itemId = favouriteButton.dataset.itemId
-    const requestInfo = {
-      method: "DELETE",
-      headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
-    }
+//   deleteFavourite(event) {
+//     event.preventDefault()
+//     const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+//     const favouriteButton = event.currentTarget
+//     const itemId = favouriteButton.dataset.itemId
+//     const requestInfo = {
+//       method: "DELETE",
+//       headers: { "Accept": "application/json", 'X-CSRF-Token': token, "Content-Type": "application/json" }
+//     }
 
-    fetch(`/favourites/${itemId}`, requestInfo)
-      .then(response => response.json())
-      .then((data) => {
-        this.element.outerHTML = data.inserted_item
-      })
-  }
-}
+//     fetch(`/favourites/${itemId}`, requestInfo)
+//       .then(response => response.json())
+//       .then((data) => {
+//         this.element.outerHTML = data.inserted_item
+//       })
+//   }
+// }
